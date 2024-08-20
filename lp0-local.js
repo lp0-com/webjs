10f01fb8171a555288f472c8c05307160767f8fa
hansel <hansel@openb.net>
2024-08-20 12:58:58 +0800

    init pub js

---
+++ lp0-local.js
@@ -0,0 +1,143 @@
+import {
+    loadScript,
+    initializeModules,
+    initializeLp0,
+    handleLp0Subscription,
+    publishMessageToLp0,
+  } from "./lp0_utils-local.js";
+  
+  class LP0WebChat extends HTMLElement {
+    constructor() {
+      super();
+      this.initialized = false;
+      console.log("LP0 WebChat: Constructor initialized.");
+    }
+  
+    // Validates required attributes and logs errors if any are missing
+    validateAttributes() {
+      const requiredAttributes = [
+        "bot-id",
+        "customer-id",
+        "user-message-classes",
+        "bot-message-classes",
+        "user-align-classes",
+        "bot-align-classes",
+        "container-classes",
+        "input-classes",
+        "send-button-classes",
+        "input-wrapper-classes",
+        "theme-config",
+        "placeholder-text", // New attribute for placeholder text
+        "show-history",
+        "hide-start"
+      ];
+  
+      return requiredAttributes.every(attr => {
+        if (!this.hasAttribute(attr)) {
+          console.error(`LP0 WebChat: Missing required attribute - ${attr}`);
+          return false;
+        }
+        return true;
+      });
+    }
+  
+    async connectedCallback() {
+      if (this.initialized) {
+        console.log("LP0 WebChat: Already initialized.");
+        return;
+      }
+  
+      if (!this.validateAttributes()) {
+        console.error("LP0 WebChat: Initialization aborted due to missing attributes.");
+        return;
+      }
+  
+      this.initialized = true;
+      console.log("LP0 WebChat: ConnectedCallback - Initialization starts.");
+  
+      this.setupHTMLStructure();
+      await this.initializeComponent();
+    }
+  
+    setupHTMLStructure() {
+      this.innerHTML = `
+        <div id="chat-container" class="${this.getAttribute("container-classes")}">
+          <div id="chat" class="chat-container">
+            <div id="loading" class="text-center" style="display: none;">Loading...</div>
+            <div id="error" class="text-center mb-2" style="display: none;">An error occurred. Please try again later.</div>
+            <div hx-lp0-subscribe class="chat-messages" aria-live="polite"></div>
+          </div>
+          <div class="${this.getAttribute("input-wrapper-classes")}">
+            <input type="text" id="userInput" placeholder="${this.getAttribute("placeholder-text")}" class="${this.getAttribute("input-classes")} border-none rounded-none focus:outline-none focus:ring-0" />
+            <button class="${this.getAttribute("send-button-classes")} border-none rounded-none" id="sendMessageButton">Send</button>
+          </div>
+        </div>
+      `;
+    }
+  
+    async initializeComponent() {
+      try {
+        await loadScript("https://cdn.twind.style", document.head);
+        console.log("All scripts loaded successfully.");
+  
+        const themeConfig = JSON.parse(this.getAttribute("theme-config"));
+        window.twind.install({
+          theme: {
+            extend: {
+              colors: themeConfig.colors,
+            },
+          },
+        });
+        console.log("Twind installed with user theme config.");
+  
+        await initializeModules();
+        await initializeLp0();
+  
+        const chatElement = this.querySelector("[hx-lp0-subscribe]");
+        const showHistory = this.getAttribute('show-history') === 'true';
+        const hideStart = this.getAttribute('hide-start') === 'true';
+  
+        if (chatElement) {
+          handleLp0Subscription(
+            this.getAttribute("bot-id"),
+            this.getAttribute("customer-id"),
+            chatElement,
+            this.getAttribute("user-align-classes"),
+            this.getAttribute("user-message-classes"),
+            this.getAttribute("bot-align-classes"),
+            this.getAttribute("bot-message-classes"),
+            hideStart,
+            showHistory
+          );
+        }
+  
+        const sendMessageButton = this.querySelector("#sendMessageButton");
+        const userInput = this.querySelector("#userInput");
+  
+        // Trigger send message on Enter key press
+        userInput.addEventListener("keydown", (event) => {
+          if (event.key === "Enter") {
+            sendMessageButton.click();
+          }
+        });
+  
+        sendMessageButton.addEventListener(
+          "click",
+          async () => {
+            const userInputValue = userInput.value;
+            await publishMessageToLp0(
+              this.getAttribute("bot-id"),
+              this.getAttribute("customer-id"),
+              userInputValue,
+            );
+            userInput.value = ""; // Clear the input field after sending the message
+          },
+        );
+      } catch (error) {
+        console.error("Error during LP0 WebChat initialization:", error);
+      }
+    }
+  }
+  
+  customElements.define("lp0-webchat", LP0WebChat);
+  
---
+++ lp0_utils.js
@@ -0,0 +1,192 @@
+export async function loadScript(src, shadowRoot) {
+    return new Promise((resolve, reject) => {
+      const script = document.createElement("script");
+      script.src = src;
+      script.onload = resolve;
+      script.onerror = () => reject(new Error(`Failed to load script: ${src}`));
+      shadowRoot.appendChild(script);
+    });
+  }
+  
+  export async function initializeModules() {
+    try {
+      console.log("Loading external modules...");
+  
+      const bufferModule = await import(
+        "https://cdn.jsdelivr.net/npm/buffer@6.0.3/+esm"
+      );
+      window.Buffer = bufferModule.Buffer;
+      if (!window.Buffer) throw new Error("Buffer module failed to load.");
+      console.log("Buffer module loaded.");
+  
+      window.nats = await import(
+        "https://cdn.jsdelivr.net/npm/nats.ws@1.29.2/+esm"
+      );
+      if (!window.nats) throw new Error("NATS module failed to load.");
+      console.log("NATS module loaded.");
+  
+      window.nkeys = (
+        await import("https://cdn.jsdelivr.net/npm/ts-nkeys@1.0.16/+esm")
+      ).default;
+      if (!window.nkeys) throw new Error("nkeys module failed to load.");
+      console.log("nkeys module loaded.");
+  
+      const uuidModule = await import(
+        "https://cdn.jsdelivr.net/npm/uuid@10.0.0/+esm"
+      );
+      window.uuidv4 = uuidModule.v4;
+      if (!window.uuidv4) throw new Error("UUID module failed to load.");
+      console.log("UUID module loaded.");
+  
+      window.sc = window.nats.StringCodec();
+      if (!window.sc) throw new Error("StringCodec failed to initialize.");
+      console.log("StringCodec initialized.");
+    } catch (error) {
+      console.error("Error initializing modules:", error);
+      throw error;
+    }
+  }
+  
+  export async function getJwt(publicKey) {
+    try {
+      console.log("Fetching JWT for public key:", publicKey);
+      const response = await fetch(
+        `https://jwt.lp0.com/jwt/user?public_key=${encodeURIComponent(publicKey)}`,
+      );
+      if (!response.ok) {
+        throw new Error(`Failed to get JWT: ${response.statusText}`);
+      }
+      const data = await response.json();
+      if (!data.jwt) throw new Error("JWT token is missing in the response.");
+      console.log("JWT fetched successfully.");
+      return data.jwt;
+    } catch (error) {
+      console.error("Error fetching JWT:", error);
+      throw error;
+    }
+  }
+  
+  export async function initializeLp0() {
+    try {
+      console.log("Initializing LP0...");
+  
+      window.lp0_session_id = window.uuidv4();
+      if (!window.lp0_session_id)
+        throw new Error("Failed to generate session ID.");
+      console.log("Session ID generated:", window.lp0_session_id);
+  
+      const seed =
+        localStorage.getItem("custSeed") ||
+        window.nkeys.createUser().getSeed().toString();
+      localStorage.setItem("custSeed", seed);
+  
+      const keypair = window.nkeys.fromSeed(Buffer.from(seed));
+      window.publicKey = keypair.getPublicKey().toString();
+      if (!window.publicKey) throw new Error("Failed to generate public key.");
+      console.log("Public key generated:", window.publicKey);
+  
+      const fetchedJwt = await getJwt(window.publicKey);
+      if (!fetchedJwt) throw new Error("Failed to fetch JWT.");
+  
+      const options = {
+        servers: "wss://nats.lp0.com",
+        authenticator: window.nats.jwtAuthenticator(
+          fetchedJwt,
+          new Uint8Array(keypair.getSeed()),
+        ),
+        inboxPrefix: `_INBOX.${keypair.getPublicKey().toString()}`,
+        timeout: 60000,
+      };
+  
+      const nc = await window.nats.connect(options);
+      const js = nc.jetstream();
+  
+      window.nc = nc;
+      window.js = js;
+      if (!window.nc || !window.js)
+        throw new Error("Failed to establish NATS connection.");
+      console.log("NATS connection established.");
+    } catch (error) {
+      console.error("Error initializing LP0:", error);
+      throw error;
+    }
+  }
+  
+  export async function handleLp0Subscription(
+    botId,
+    customerId,
+    chatElement,
+    userAlignClasses,
+    userMessageClasses,
+    botAlignClasses,
+    botMessageClasses,
+    hideStart, // existing parameter
+    showHistory // new parameter
+  ) {
+    try {
+      console.log("Handling LP0 subscription for botId:", botId, "customerId:", customerId);
+  
+      if (!window.nc || !window.js) {
+        throw new Error("NATS connection (nc or js) is not established.");
+      }
+  
+      const sessionId = window.lp0_session_id;
+      const userPublicKey = window.publicKey;
+  
+      const userSub = window.nc.subscribe(`user.${userPublicKey}.${sessionId}.user`);
+      (async () => {
+        for await (const m of userSub) {
+          const message = window.sc.decode(m.data);
+          if (!message) throw new Error("Failed to decode user message.");
+          console.log("Received user message:", message);
+          if (message === "/start" && hideStart) {
+            continue;
+          };
+          // Only display user messages if showHistory is true
+          if (showHistory) {
+            chatElement.innerHTML += `<div class="${userAlignClasses}"><div class="${userMessageClasses}">${message}</div></div>`;
+          }
+        }
+      })().catch(console.error);
+  
+      const botSub = window.nc.subscribe(`user.${userPublicKey}.${sessionId}.bot.${botId}`);
+      (async () => {
+        for await (const m of botSub) {
+          const message = window.sc.decode(m.data);
+          if (!message) throw new Error("Failed to decode bot message.");
+          console.log("Received bot message:", message);
+  
+          if (!showHistory) {
+            // Clear existing messages and only show the latest bot message if showHistory is false
+            chatElement.innerHTML = `<div class="${botAlignClasses}"><div class="${botMessageClasses}">${message}</div></div>`;
+          } else {
+            // Add new bot message to the chat
+            chatElement.innerHTML += `<div class="${botAlignClasses}"><div class="${botMessageClasses}">${message}</div></div>`;
+          }
+        }
+      })().catch(console.error);
+  
+      await publishMessageToLp0(botId, customerId, "/start");
+      console.log('"/start" message sent.');
+    } catch (error) {
+      console.error("Error handling LP0 subscription:", error);
+      throw error;
+    }
+  }
+  
+  
+  export async function publishMessageToLp0(botId, customerId, message) {
+    try {
+      if (!botId || !customerId || !message)
+        throw new Error("Bot ID, Customer ID, and message are required.");
+      const sessionId = window.lp0_session_id;
+      const subject = `service.chat.${window.publicKey}.${sessionId}.${botId}.${customerId}`;
+      console.log(`Publishing message to subject: ${subject}`);
+      await window.nc.publish(subject, window.sc.encode(message));
+      console.log("Message published successfully.");
+    } catch (error) {
+      console.error("Error publishing LP0 message:", error);
+      throw error;
+    }
+  }
+  
